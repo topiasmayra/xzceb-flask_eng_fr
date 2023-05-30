ee8d0c6ad2b41736768c88afd507c27dd2f84061
your_github_username <topias.mayra@gmail.com>
2023-05-30 12:01:30 -0400

    Final changes

--- final_project/machinetranslation/server.py
+++ final_project/machinetranslation/server.py
@@ -1,6 +1,5 @@
-
 from flask import Flask, render_template, request
-from machinetranslation  import english_to_french, french_to_english
+from machinetranslation import english_to_french, french_to_english
 
 app = Flask(__name__)
 
@@ -11,13 +10,13 @@ def index():
 @app.route('/englishToFrench', methods=['POST'])
 def translate_english_to_french():
     english_text = request.form['english_text']
-    french_text = english_to_french(english_text)
+    french_text = english_to_french.translate(english_text)
     return french_text
 
 @app.route('/frenchToEnglish', methods=['POST'])
 def translate_french_to_english():
     french_text = request.form['french_text']
-    english_text = french_to_english(french_text)
+    english_text = french_to_english.translate(french_text)
     return english_text
 
 if __name__ == '__main__':
